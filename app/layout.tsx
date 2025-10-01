99a3ab839c11e8ccf34b976ec6a764b0a1bd4312
Enzo Vezzaro <enzo-vezzaro@live.it>
2025-10-01 17:45:54 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
 					try {
 						return (
 						<Script
-							async
+							strategy="afterInteractive"
 							src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
 						/>
 						);
